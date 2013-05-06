5e1cfb6e82810e9325bdb7b308f554f5da917f44
Janusz Slota <janusz.slota@nixilla.com>
2013-05-06 14:58:53 +0100

    Added navigation

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -10,4 +10,9 @@ class DefaultController extends Controller
     {
         return $this->render('WurstpressCoreBundle:Default:index.html.twig');
     }
+
+    public function navigationAction()
+    {
+        return $this->render('WurstpressCoreBundle:Default:navigation.html.twig');
+    }
 }
