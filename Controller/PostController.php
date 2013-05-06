57871fb928c713769b71bc2f2da7897645230d91
Janusz Slota <janusz.slota@nixilla.com>
2013-05-06 12:51:45 +0100

    Added first module - Post

---
+++ Controller/PostController.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace Wurstpress\CoreBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Wurstpress\CoreBundle\Entity\Post;
+use Wurstpress\CoreBundle\Form\PostType;
+
+/**
+ * Post controller.
+ *
+ */
+class PostController extends Controller
+{
+    /**
+     * Lists all Post entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('WurstpressCoreBundle:Post')->findAll();
+
+        return $this->render('WurstpressCoreBundle:Post:index.html.twig', array(
+            'entities' => $entities,
+        ));
+    }
+
+    /**
+     * Creates a new Post entity.
+     *
+     */
+    public function createAction(Request $request)
+    {
+        $entity  = new Post();
+        $form = $this->createForm(new PostType(), $entity);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('post_show', array('id' => $entity->getId())));
+        }
+
+        return $this->render('WurstpressCoreBundle:Post:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to create a new Post entity.
+     *
+     */
+    public function newAction()
+    {
+        $entity = new Post();
+        $form   = $this->createForm(new PostType(), $entity);
+
+        return $this->render('WurstpressCoreBundle:Post:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Post entity.
+     *
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('WurstpressCoreBundle:Post')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Post entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('WurstpressCoreBundle:Post:show.html.twig', array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Post entity.
+     *
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('WurstpressCoreBundle:Post')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Post entity.');
+        }
+
+        $editForm = $this->createForm(new PostType(), $entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('WurstpressCoreBundle:Post:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Edits an existing Post entity.
+     *
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('WurstpressCoreBundle:Post')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Post entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createForm(new PostType(), $entity);
+        $editForm->bind($request);
+
+        if ($editForm->isValid()) {
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('post_edit', array('id' => $id)));
+        }
+
+        return $this->render('WurstpressCoreBundle:Post:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Post entity.
+     *
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('WurstpressCoreBundle:Post')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Post entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('post'));
+    }
+
+    /**
+     * Creates a form to delete a Post entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder(array('id' => $id))
+            ->add('id', 'hidden')
+            ->getForm()
+        ;
+    }
+}
---
+++ Form/PostType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Wurstpress\CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Wurstpress\CoreBundle\Entity\Post'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'post';
+    }
+}
